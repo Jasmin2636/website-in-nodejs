368f8d4ea0d60318ae60d7d050dde373d2945467
Jasmin TA <jasminjasmi222@gmail.com>
2023-10-30 15:30:29 +0530

    Jasmin2636/website-in-nodejs

--- website.js
+++ website.js
@@ -1,12 +1,4 @@
-/*var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('website.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8089);*/
+
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
